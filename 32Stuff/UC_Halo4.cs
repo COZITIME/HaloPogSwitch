ccc67014bc0805b855b8a39ee1bf43009f8282d8
Jayden Cosgrove <Magic.turd@hotmail.com>
2021-01-27 15:51:31 +1000

    >Moved things around a bit in hopes to get porn hub working >Moved new Ui Stuff to the 32 Bit Project

---
+++ 32Stuff/UC_Halo4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HaloPogSwitch
+{
+    public partial class UC_Halo4 : UserControl
+    {
+        public UC_Halo4()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 32Stuff/UC_HaloReach.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HaloPogSwitch
+{
+    public partial class UC_HaloReach : UserControl
+    {
+        public UC_HaloReach()
+        {
+            InitializeComponent();
+        }
+    }
+}
