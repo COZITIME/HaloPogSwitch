a79f8777f7f52fb3c4630d67fbb3eeb26390a854
Seraphicy <codenamezatox@gmail.com>
2021-01-27 13:01:06 -0600

    Fixed some typos and made the UI a bit bigger

--- HaloPogSwitch/MyForm.Designer.cs
+++ HaloPogSwitch/MyForm.Designer.cs
@@ -72,22 +72,22 @@ namespace HaloPogSwitch
             this.siticonePanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.siticonePanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
             this.siticonePanel1.Location = new System.Drawing.Point(0, 0);
-            this.siticonePanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.siticonePanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.siticonePanel1.Name = "siticonePanel1";
             this.siticonePanel1.ShadowDecoration.Depth = 5;
             this.siticonePanel1.ShadowDecoration.Enabled = true;
             this.siticonePanel1.ShadowDecoration.Parent = this.siticonePanel1;
             this.siticonePanel1.ShadowDecoration.Shadow = new System.Windows.Forms.Padding(3, 3, 5, 3);
-            this.siticonePanel1.Size = new System.Drawing.Size(142, 467);
+            this.siticonePanel1.Size = new System.Drawing.Size(206, 575);
             this.siticonePanel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 305);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 327);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(145, 162);
+            this.pictureBox1.Size = new System.Drawing.Size(206, 248);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -95,12 +95,11 @@ namespace HaloPogSwitch
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(3, 37);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(9, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 30);
+            this.label1.Size = new System.Drawing.Size(189, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "HaloPogSwitch";
             // 
@@ -114,17 +113,17 @@ namespace HaloPogSwitch
             this.halo2a.CustomBorderThickness = new System.Windows.Forms.Padding(5, 0, 0, 0);
             this.halo2a.CustomImages.Parent = this.halo2a;
             this.halo2a.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
-            this.halo2a.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.halo2a.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.halo2a.ForeColor = System.Drawing.Color.White;
             this.halo2a.HoveredState.Parent = this.halo2a;
             this.halo2a.Image = ((System.Drawing.Image)(resources.GetObject("halo2a.Image")));
             this.halo2a.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.halo2a.Location = new System.Drawing.Point(0, 184);
-            this.halo2a.Margin = new System.Windows.Forms.Padding(2);
+            this.halo2a.Location = new System.Drawing.Point(0, 226);
+            this.halo2a.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.halo2a.Name = "halo2a";
             this.halo2a.PressedDepth = 0;
             this.halo2a.ShadowDecoration.Parent = this.halo2a;
-            this.halo2a.Size = new System.Drawing.Size(142, 32);
+            this.halo2a.Size = new System.Drawing.Size(189, 39);
             this.halo2a.TabIndex = 4;
             this.halo2a.Text = "Halo 2A";
             this.halo2a.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
@@ -141,17 +140,17 @@ namespace HaloPogSwitch
             this.halo3.CustomBorderThickness = new System.Windows.Forms.Padding(5, 0, 0, 0);
             this.halo3.CustomImages.Parent = this.halo3;
             this.halo3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
-            this.halo3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.halo3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.halo3.ForeColor = System.Drawing.Color.White;
             this.halo3.HoveredState.Parent = this.halo3;
             this.halo3.Image = ((System.Drawing.Image)(resources.GetObject("halo3.Image")));
             this.halo3.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.halo3.Location = new System.Drawing.Point(0, 152);
-            this.halo3.Margin = new System.Windows.Forms.Padding(2);
+            this.halo3.Location = new System.Drawing.Point(0, 187);
+            this.halo3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.halo3.Name = "halo3";
             this.halo3.PressedDepth = 0;
             this.halo3.ShadowDecoration.Parent = this.halo3;
-            this.halo3.Size = new System.Drawing.Size(142, 32);
+            this.halo3.Size = new System.Drawing.Size(189, 39);
             this.halo3.TabIndex = 3;
             this.halo3.Text = "Halo 3";
             this.halo3.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
@@ -169,17 +168,17 @@ namespace HaloPogSwitch
             this.halo4.CustomBorderThickness = new System.Windows.Forms.Padding(5, 0, 0, 0);
             this.halo4.CustomImages.Parent = this.halo4;
             this.halo4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
-            this.halo4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.halo4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.halo4.ForeColor = System.Drawing.Color.White;
             this.halo4.HoveredState.Parent = this.halo4;
             this.halo4.Image = ((System.Drawing.Image)(resources.GetObject("halo4.Image")));
             this.halo4.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.halo4.Location = new System.Drawing.Point(0, 120);
-            this.halo4.Margin = new System.Windows.Forms.Padding(2);
+            this.halo4.Location = new System.Drawing.Point(0, 148);
+            this.halo4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.halo4.Name = "halo4";
             this.halo4.PressedDepth = 0;
             this.halo4.ShadowDecoration.Parent = this.halo4;
-            this.halo4.Size = new System.Drawing.Size(142, 32);
+            this.halo4.Size = new System.Drawing.Size(189, 39);
             this.halo4.TabIndex = 3;
             this.halo4.Text = "Halo 4";
             this.halo4.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
@@ -197,17 +196,17 @@ namespace HaloPogSwitch
             this.haloreach.CustomBorderThickness = new System.Windows.Forms.Padding(5, 0, 0, 0);
             this.haloreach.CustomImages.Parent = this.haloreach;
             this.haloreach.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
-            this.haloreach.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.haloreach.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.haloreach.ForeColor = System.Drawing.Color.White;
             this.haloreach.HoveredState.Parent = this.haloreach;
             this.haloreach.Image = ((System.Drawing.Image)(resources.GetObject("haloreach.Image")));
             this.haloreach.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.haloreach.Location = new System.Drawing.Point(0, 88);
-            this.haloreach.Margin = new System.Windows.Forms.Padding(2);
+            this.haloreach.Location = new System.Drawing.Point(0, 108);
+            this.haloreach.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.haloreach.Name = "haloreach";
             this.haloreach.PressedDepth = 0;
             this.haloreach.ShadowDecoration.Parent = this.haloreach;
-            this.haloreach.Size = new System.Drawing.Size(142, 32);
+            this.haloreach.Size = new System.Drawing.Size(189, 39);
             this.haloreach.TabIndex = 2;
             this.haloreach.Text = "Halo: Reach";
             this.haloreach.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
@@ -220,11 +219,11 @@ namespace HaloPogSwitch
             | System.Windows.Forms.AnchorStyles.Right)));
             this.siticonePanel2.Controls.Add(this.siticoneControlBox2);
             this.siticonePanel2.Controls.Add(this.siticoneControlBox1);
-            this.siticonePanel2.Location = new System.Drawing.Point(147, 0);
-            this.siticonePanel2.Margin = new System.Windows.Forms.Padding(2);
+            this.siticonePanel2.Location = new System.Drawing.Point(212, 0);
+            this.siticonePanel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.siticonePanel2.Name = "siticonePanel2";
             this.siticonePanel2.ShadowDecoration.Parent = this.siticonePanel2;
-            this.siticonePanel2.Size = new System.Drawing.Size(526, 32);
+            this.siticonePanel2.Size = new System.Drawing.Size(803, 39);
             this.siticonePanel2.TabIndex = 5;
             // 
             // siticoneControlBox2
@@ -234,11 +233,11 @@ namespace HaloPogSwitch
             this.siticoneControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
             this.siticoneControlBox2.HoveredState.Parent = this.siticoneControlBox2;
             this.siticoneControlBox2.IconColor = System.Drawing.Color.White;
-            this.siticoneControlBox2.Location = new System.Drawing.Point(459, 0);
-            this.siticoneControlBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.siticoneControlBox2.Location = new System.Drawing.Point(714, 0);
+            this.siticoneControlBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.siticoneControlBox2.Name = "siticoneControlBox2";
             this.siticoneControlBox2.ShadowDecoration.Parent = this.siticoneControlBox2;
-            this.siticoneControlBox2.Size = new System.Drawing.Size(34, 24);
+            this.siticoneControlBox2.Size = new System.Drawing.Size(45, 30);
             this.siticoneControlBox2.TabIndex = 7;
             // 
             // siticoneControlBox1
@@ -249,11 +248,11 @@ namespace HaloPogSwitch
             this.siticoneControlBox1.HoveredState.IconColor = System.Drawing.Color.White;
             this.siticoneControlBox1.HoveredState.Parent = this.siticoneControlBox1;
             this.siticoneControlBox1.IconColor = System.Drawing.Color.White;
-            this.siticoneControlBox1.Location = new System.Drawing.Point(493, 0);
-            this.siticoneControlBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.siticoneControlBox1.Location = new System.Drawing.Point(759, 0);
+            this.siticoneControlBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.siticoneControlBox1.Name = "siticoneControlBox1";
             this.siticoneControlBox1.ShadowDecoration.Parent = this.siticoneControlBox1;
-            this.siticoneControlBox1.Size = new System.Drawing.Size(34, 24);
+            this.siticoneControlBox1.Size = new System.Drawing.Size(45, 30);
             this.siticoneControlBox1.TabIndex = 6;
             // 
             // siticoneDragControl1
@@ -269,11 +268,11 @@ namespace HaloPogSwitch
             this.siticonePanel3.Controls.Add(this.uC_Halo31);
             this.siticonePanel3.Controls.Add(this.uC_Halo41);
             this.siticonePanel3.Controls.Add(this.uC_Halo2a1);
-            this.siticonePanel3.Location = new System.Drawing.Point(147, 37);
-            this.siticonePanel3.Margin = new System.Windows.Forms.Padding(2);
+            this.siticonePanel3.Location = new System.Drawing.Point(212, 46);
+            this.siticonePanel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.siticonePanel3.Name = "siticonePanel3";
             this.siticonePanel3.ShadowDecoration.Parent = this.siticonePanel3;
-            this.siticonePanel3.Size = new System.Drawing.Size(526, 430);
+            this.siticonePanel3.Size = new System.Drawing.Size(804, 529);
             this.siticonePanel3.TabIndex = 6;
             // 
             // uC_HaloReach1
@@ -281,9 +280,9 @@ namespace HaloPogSwitch
             this.uC_HaloReach1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
             this.uC_HaloReach1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.uC_HaloReach1.Location = new System.Drawing.Point(0, 0);
-            this.uC_HaloReach1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.uC_HaloReach1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.uC_HaloReach1.Name = "uC_HaloReach1";
-            this.uC_HaloReach1.Size = new System.Drawing.Size(526, 430);
+            this.uC_HaloReach1.Size = new System.Drawing.Size(804, 529);
             this.uC_HaloReach1.TabIndex = 0;
             // 
             // uC_Halo31
@@ -291,9 +290,9 @@ namespace HaloPogSwitch
             this.uC_Halo31.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
             this.uC_Halo31.Dock = System.Windows.Forms.DockStyle.Fill;
             this.uC_Halo31.Location = new System.Drawing.Point(0, 0);
-            this.uC_Halo31.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.uC_Halo31.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.uC_Halo31.Name = "uC_Halo31";
-            this.uC_Halo31.Size = new System.Drawing.Size(526, 430);
+            this.uC_Halo31.Size = new System.Drawing.Size(804, 529);
             this.uC_Halo31.TabIndex = 4;
             // 
             // uC_Halo41
@@ -301,9 +300,9 @@ namespace HaloPogSwitch
             this.uC_Halo41.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
             this.uC_Halo41.Dock = System.Windows.Forms.DockStyle.Fill;
             this.uC_Halo41.Location = new System.Drawing.Point(0, 0);
-            this.uC_Halo41.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.uC_Halo41.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.uC_Halo41.Name = "uC_Halo41";
-            this.uC_Halo41.Size = new System.Drawing.Size(526, 430);
+            this.uC_Halo41.Size = new System.Drawing.Size(804, 529);
             this.uC_Halo41.TabIndex = 3;
             // 
             // uC_Halo2a1
@@ -311,22 +310,23 @@ namespace HaloPogSwitch
             this.uC_Halo2a1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
             this.uC_Halo2a1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.uC_Halo2a1.Location = new System.Drawing.Point(0, 0);
-            this.uC_Halo2a1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.uC_Halo2a1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.uC_Halo2a1.Name = "uC_Halo2a1";
-            this.uC_Halo2a1.Size = new System.Drawing.Size(526, 430);
+            this.uC_Halo2a1.Size = new System.Drawing.Size(804, 529);
             this.uC_Halo2a1.TabIndex = 5;
             // 
             // MyForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
-            this.ClientSize = new System.Drawing.Size(673, 467);
+            this.ClientSize = new System.Drawing.Size(1016, 575);
             this.Controls.Add(this.siticonePanel3);
             this.Controls.Add(this.siticonePanel2);
             this.Controls.Add(this.siticonePanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MyForm";
             this.Text = "HaloPogSwitch";
             this.Load += new System.EventHandler(this.FuckingCunt_Load);
