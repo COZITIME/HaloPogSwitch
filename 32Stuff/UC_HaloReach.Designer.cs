6e108282d315b0b41f644f5c1f981530c5d47714
Jayden Cosgrove <Magic.turd@hotmail.com>
2021-01-27 15:51:31 +1000

    >Moved new Ui Stuff to the 32 Bit Project

---
+++ 32Stuff/UC_HaloReach.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace HaloPogSwitch
+{
+    partial class UC_HaloReach
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.siticoneLabel1 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.SuspendLayout();
+            // 
+            // siticoneLabel1
+            // 
+            this.siticoneLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel1.Font = new System.Drawing.Font("Segoe UI Light", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneLabel1.ForeColor = System.Drawing.Color.White;
+            this.siticoneLabel1.Location = new System.Drawing.Point(20, 10);
+            this.siticoneLabel1.Name = "siticoneLabel1";
+            this.siticoneLabel1.Size = new System.Drawing.Size(167, 47);
+            this.siticoneLabel1.TabIndex = 0;
+            this.siticoneLabel1.Text = "Halo: Reach";
+            // 
+            // UC_HaloReach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.Controls.Add(this.siticoneLabel1);
+            this.Name = "UC_HaloReach";
+            this.Size = new System.Drawing.Size(630, 457);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel1;
+    }
+}
