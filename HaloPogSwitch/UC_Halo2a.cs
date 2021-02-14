b638a466e8c1f8e65df20a5ec7ceb17c4019ef32
Jayden Cosgrove <Magic.turd@hotmail.com>
2021-02-14 17:53:48 +1000

    Revert "Revert ">h2a/h4 ui basically done and working""
    This reverts commit e376921144ec5883dff170d068f75c241a98085f.

--- HaloPogSwitch/UC_Halo2a.cs
+++ HaloPogSwitch/UC_Halo2a.cs
@@ -10,31 +10,60 @@ using System.Windows.Forms;
 
 namespace HaloPogSwitch
 {
-    public partial class UC_Halo2a : UserControl, IHaloUserControls<H2ATab>
+    public partial class UC_Halo2a : UserControl
     {
         public UC_Halo2a()
         {
             InitializeComponent();
         }
 
-        public ControlCollection GetControls(H2ATab numnum)
+        public FlowLayoutPanel GetHalo2AFlow(H2ATab numnum)
         {
             switch (numnum)
             {
-                case H2ATab.visor:
-                   
-                    break;
+                case H2ATab.helmateVisor:
+                    return FlowLayout_Helmet;
+                case H2ATab.chest:
+                    return FlowLayout_Chest;
+                case H2ATab.shoulder:
+                    return FlowLayout_Shoulder;
+                case H2ATab.wrist:
+                    return FlowLayout_Wrist;
+                case H2ATab.knees:
+                    return FlowLayout_Knee;
+                case H2ATab.serviceid:
+                    return FlowLayout_ServiceID;
+                case H2ATab.color:
+                    return FlowLayout_Color;
+                case H2ATab.elitearmor:
+                    return FlowLayout_EliteArmor;
+                case H2ATab.species:
+                    return FlowLayout_Species;
+                case H2ATab.gender:
+                    return FlowLayout_Gender;
                 default:
-                    break;
+                    return null;
             }
+        }
 
-            return flowLayoutPanel1.Controls;
+        private void UC_Halo2a_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
 
 public enum H2ATab
 {
-    visor
+    helmateVisor,
+    shoulder,
+    chest,
+    wrist,
+    knees,
+    color,
+    elitearmor,
+    species,
+    gender,
+    serviceid,
 
 }
