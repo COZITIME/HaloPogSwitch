07e60caf8613c5ff2d6e8786b11d73f2bb374489
Jayden Cosgrove <Magic.turd@hotmail.com>
2021-02-15 14:00:46 +1000

    > Added Credits to welcome page

--- HaloPogSwitch/UC_Welcome.Designer.cs
+++ HaloPogSwitch/UC_Welcome.Designer.cs
@@ -30,28 +30,48 @@ namespace HaloPog
         private void InitializeComponent()
         {
             this.siticoneLabel1 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // siticoneLabel1
             // 
             this.siticoneLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.siticoneLabel1.Font = new System.Drawing.Font("Segoe UI Semilight", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.siticoneLabel1.ForeColor = System.Drawing.Color.White;
-            this.siticoneLabel1.Location = new System.Drawing.Point(20, 10);
+            this.siticoneLabel1.Location = new System.Drawing.Point(0, 0);
+            this.siticoneLabel1.Margin = new System.Windows.Forms.Padding(2);
             this.siticoneLabel1.Name = "siticoneLabel1";
-            this.siticoneLabel1.Size = new System.Drawing.Size(494, 56);
+            this.siticoneLabel1.Size = new System.Drawing.Size(964, 47);
             this.siticoneLabel1.TabIndex = 2;
             this.siticoneLabel1.Text = "Welcome to HaloPogSwitch!";
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedHorizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(964, 596);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "To get started Launch Halo: Reach, Halo 4 or Halo 2: Anniversary. ";
+            // 
             // UC_Welcome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.siticoneLabel1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UC_Welcome";
-            this.Size = new System.Drawing.Size(558, 386);
+            this.Size = new System.Drawing.Size(964, 643);
+            this.Load += new System.EventHandler(this.UC_Welcome_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,5 +80,6 @@ namespace HaloPog
         #endregion
 
         private Siticone.UI.WinForms.SiticoneLabel siticoneLabel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
